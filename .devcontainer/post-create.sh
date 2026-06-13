01d920199c181bc862140d4bb689ab834ad92f72
Kevin Thai <kvn.thai@gmail.com>
2026-06-13 04:31:36 +0000

    improve development environment

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+LAZYGIT_ARCH=$(uname -m | sed -e 's/aarch64/arm64/')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_${LAZYGIT_ARCH}.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit lazygit.tar.gz
+
+# Alias
+echo "alias lg='lazygit'" >> ~/.bash_aliases
+
+# Project deps
+pipenv install --dev
